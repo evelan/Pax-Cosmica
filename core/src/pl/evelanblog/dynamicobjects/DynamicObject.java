5812a53e9c738dfeca11d02c9b45b8fda2cf2374
evelan <evelan3094@gmail.com>
2014-12-31 00:51:52 +0100

    przed mergem

--- core/src/pl/evelanblog/dynamicobjects/DynamicObject.java
+++ core/src/pl/evelanblog/dynamicobjects/DynamicObject.java
@@ -7,76 +7,71 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class DynamicObject extends Sprite {
 
-	protected float hp;
-	protected boolean live;
-	protected float speed;
-	protected float impactDamage; // im wyższa tym większe obrażenia będą zadawane przy uderzeniu
-	protected float shield;
+    protected float hp;
+    protected boolean live;
+    protected float speed;
+    protected float impactDamage; // im wyższa tym większe obrażenia będą zadawane przy uderzeniu
+    protected float shield;
 
-	public DynamicObject(float x, float y, float speed, float hp, float shield, float impactDamage, String file)
-	{
-		super(new Texture(Gdx.files.internal(file)));
-		setBounds(x, y, getTexture().getWidth(), getTexture().getHeight());
-		setOriginCenter();
-		live = true;
-		this.speed = speed;
-		this.impactDamage = impactDamage;
-		this.shield = shield;
-		this.hp = hp;
-	}
+    public DynamicObject(float x, float y, float speed, float hp, float shield, float impactDamage, String file) {
+        super(new Texture(Gdx.files.internal(file)));
+        setBounds(x, y, getTexture().getWidth(), getTexture().getHeight());
+        setOriginCenter();
+        live = true;
+        this.speed = speed;
+        this.impactDamage = impactDamage;
+        this.shield = shield;
+        this.hp = hp;
+    }
 
-	public void setTexture(String file)
-	{
-		setTexture(new Texture(Gdx.files.internal(file)));
-		setSize(getTexture().getWidth(), getTexture().getHeight());
-		setOriginCenter();
-	}
+    public void setTexture(String file) {
+        setTexture(new Texture(Gdx.files.internal(file)));
+        setSize(getTexture().getWidth(), getTexture().getHeight());
+        setOriginCenter();
+    }
 
-	public float getImpactDamage()
-	{
-		return impactDamage;
-	}
+    public float getImpactDamage() {
+        return impactDamage;
+    }
 
-	public float getSpeed() {
-		return speed;
-	}
+    public float getSpeed() {
+        return speed;
+    }
 
-	public float getHealth() {
-		return hp;
-	}
+    public float getHealth() {
+        return hp;
+    }
 
-	public float getShield()
-	{
-		return shield;
-	}
+    public float getShield() {
+        return shield;
+    }
 
-	/**
-	 * Uszkadza obiekt, jak HP spadnie poniżej 0 to umiera
-	 * 
-	 * @param damage
-	 *            ilość zadawanych obrażeń
-	 */
-	public void hurt(float damage) {
+    /**
+     * Uszkadza obiekt, jak HP spadnie poniżej 0 to umiera
+     *
+     * @param damage ilość zadawanych obrażeń
+     */
+    public void hurt(float damage) {
 
-		if (shield > 0) {
-			shield -= damage;
-			shield = 0;
-		} else {
-			hp -= damage;
-		}
+        if (shield > 0) {
+            shield -= damage;
+            shield = 0;
+        } else {
+            hp -= damage;
+        }
 
-		if (hp <= 0)
-			kill();
-	}
+        if (hp <= 0)
+            kill();
+    }
 
-	public abstract void kill();
+    public abstract void kill();
 
-	public boolean isAlive() {
-		return live;
-	}
+    public boolean isAlive() {
+        return live;
+    }
 
-	public void draw(SpriteBatch batch, float delta) {
-		draw(batch);
-	}
+    public void draw(SpriteBatch batch, float delta) {
+        draw(batch);
+    }
 
 }
